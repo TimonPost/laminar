f92bd435ca713936c01503c8c9c97b5eb1dd2a80
TimonPost <timonpost@hotmail.nl>
2018-09-25 20:55:56 +0200

    removed old code

--- src/net/reliable_udp/mod.rs
+++
@@ -1,9 +0,0 @@
-mod connection;
-mod packet;
-mod udp;
-
-use self::connection::{Connection, ConnectionQuality };
-use self::packet::{ AckRecord, ExternalAcks };
-use Packet;
-
-pub use self::udp::UdpSocket;
