26cfb82f60c8ed50572ab1aff8e7496062e5ce2d
timon <timonpost@hotmail.nl>
2018-10-07 23:43:40 +0200

    Implemented Connection Pool.

---
+++ src/net/connection/connection_pool.rs
@@ -0,0 +1,103 @@
+use super::VirtualConnection;
+use error::{NetworkError, Error,Result};
+use events::Event;
+
+use std::thread;
+use std::sync::mpsc::Sender;
+use std::time::Duration;
+use std::sync::{Arc, Mutex, RwLock, RwLockWriteGuard};
+use std::net::{SocketAddr};
+use std::collections::HashMap;
+
+type Connection = Arc<RwLock<VirtualConnection>>;
+type Connections = HashMap<SocketAddr, Connection>;
+type ConnectionsCollection = Arc<RwLock<Connections>>;
+
+// Default time between checks of all clients for timeouts in seconds
+const TIMEOUT_POLL_INTERVAL: u64 = 1;
+
+/// This is an pool of virtual connections (connected) over UDP.
+pub struct ConnectionPool
+{
+    timeout: Duration,
+    connections: ConnectionsCollection,
+    sleepy_time: Duration,
+    poll_interval: Duration,
+}
+
+impl ConnectionPool {
+    pub fn new() -> ConnectionPool
+    {
+        let sleepy_time = Duration::from_secs(1);
+        let poll_interval = Duration::from_secs(TIMEOUT_POLL_INTERVAL);
+
+        ConnectionPool { timeout: Duration::from_secs(1), connections: Arc::new(RwLock::new(HashMap::new())), sleepy_time, poll_interval }
+    }
+
+    /// Set the timeout before an client will be seen as disconnected.
+    pub fn set_timeout(&mut self, timeout: Duration)
+    {
+        self.timeout = timeout;
+    }
+
+    /// Insert connection if it does not exists.
+    pub fn get_connection_or_insert(&mut self, addr: &SocketAddr) -> Result<Connection> {
+        let mut lock = self
+            .connections
+            .write()
+            .map_err(|_| NetworkError::AddConnectionToManagerFailed)?;
+
+        let connection = lock
+            .entry(*addr)
+            .or_insert_with(|| Arc::new(RwLock::new(VirtualConnection::new(*addr))));
+
+        Ok(connection.clone())
+    }
+
+    /// Start loop that will detect if connections will are disconnected.
+    ///
+    /// This function starts a background thread that does the following:
+    /// 1. Gets a read lock on the HashMap containing all the connections
+    /// 2. Iterate through each one
+    /// 3. Check if the last time we have heard from them (received a packet from them) is greater than the amount of time considered to be a timeout
+    /// 4. If they have timed out, send a notification up the stack
+    pub fn start_time_out_loop(&self, events_sender: Sender<Event>) -> Result<thread::JoinHandle<()>>
+    {
+        let connections = self.connections.clone();
+        let poll_interval = self.poll_interval;
+        let mut sender = events_sender;
+
+        Ok(thread::Builder::new()
+            .name("check_for_timeouts".into())
+            .spawn(move ||
+                loop {
+                    match connections.read() {
+                        Ok(lock) => {
+                            ConnectionPool::check_for_timeouts(&*lock, poll_interval, &mut sender);
+                        },
+                        Err(e) => {
+                            error!("Unable to acquire read lock to check for timed out connections")
+                        }
+                    }
+                    thread::sleep(poll_interval);
+                })?
+        )
+    }
+
+    /// Check if there are any connections that have not been active for the given Duration.
+    fn check_for_timeouts(connections: &Connections, sleepy_time: Duration, events_sender: &mut Sender<Event>) {
+        for (key, value) in connections.iter() {
+            if let Ok(c) = value.read() {
+                if c.last_heard() >= sleepy_time {
+                    let event = Event::TimedOut(value.clone());
+
+                    events_sender
+                        .send(event)
+                        .expect("Unable to send disconnect event");
+
+                    error!("Client has timed out: {:?}", key);
+                }
+            }
+        }
+    }
+}
--- src/net/connection/virtual_connection.rs
+++ src/net/connection/virtual_connection.rs
@@ -1,4 +1,4 @@
-use super::{ExternalAcks, LocalAckRecord};
+use net::{ExternalAcks, LocalAckRecord, NetworkQuality};
 use packet::Packet;
 use std::fmt;
 use std::net::SocketAddr;
@@ -6,26 +6,26 @@ use std::time::{Duration, Instant};
 
 /// Contains the information about a certain 'virtual connection' over udp.
 /// This stores information about the last sequence number, dropped packages, packages waiting for acknowledgement and acknowledgements gotten from the other side.
-pub struct Connection {
+pub struct VirtualConnection {
     pub seq_num: u16,
     pub dropped_packets: Vec<Packet>,
     pub waiting_packets: LocalAckRecord,
     pub their_acks: ExternalAcks,
     pub last_heard: Instant,
     pub remote_address: SocketAddr,
-    pub quality: Quality,
+    pub quality: NetworkQuality,
 }
 
-impl Connection {
+impl VirtualConnection {
     /// Creates and returns a new Connection that wraps the provided socket address
-    pub fn new(addr: SocketAddr) -> Connection {
-        Connection {
+    pub fn new(addr: SocketAddr) -> VirtualConnection {
+        VirtualConnection {
             seq_num: 0,
             dropped_packets: Vec::new(),
             waiting_packets: Default::default(),
             their_acks: Default::default(),
             last_heard: Instant::now(),
-            quality: Quality::Good,
+            quality: NetworkQuality::Good,
             remote_address: addr,
         }
     }
@@ -37,7 +37,7 @@ impl Connection {
     }
 }
 
-impl fmt::Debug for Connection {
+impl fmt::Debug for VirtualConnection {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
@@ -45,33 +45,5 @@ impl fmt::Debug for Connection {
             self.remote_address.ip(),
             self.remote_address.port()
         )
-    }
-}
-
-// TODO:
-/// This defines whether the connection is good or bad.
-/// We should use this for handling Congestion Avoidance so that when the network of the client is bad we do not flood the router with small packets.
-///
-/// When network conditions are `Good` we send 30 packets per-second, and when network conditions are `Bad` we drop to 10 packets per-second.
-pub enum Quality {
-    Good,
-    Bad,
-}
-
-#[cfg(test)]
-mod test {
-    use net::connection::Connection;
-    use std::net::ToSocketAddrs;
-
-    static TEST_HOST_IP: &'static str = "127.0.0.1";
-    static TEST_BAD_HOST_IP: &'static str = "800.0.0.1";
-    static TEST_PORT: &'static str = "20000";
-
-    #[test]
-    fn test_create_connection() {
-        let mut addr = format!("{}:{}", TEST_HOST_IP, TEST_PORT)
-            .to_socket_addrs()
-            .unwrap();
-        let _new_conn = Connection::new(addr.next().unwrap());
     }
 }
