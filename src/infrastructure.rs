70a5d48a1c26883b7cb31e5241386ab2f4d077c8
Timon Post <timonpost@hotmail.nl>
2019-02-27 20:28:51 +0100

    a

--- src/infrastructure.rs
+++ src/infrastructure.rs
@@ -1,9 +1,6 @@
 mod channels;
-///! This module provides infrastructure logic. With infrastructure is meant, everything that's responsible for the packet flow and processing.
 mod delivery_method;
 mod fragmenter;
-
-pub mod arranging;
 
 pub use self::channels::Channel;
 pub use self::channels::{ReliableChannel, SequencedChannel, UnreliableChannel};
