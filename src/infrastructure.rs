5243df6503e924263ddfae9d1b8b5522ad228a6c
Justin LeFebvre <jstnlefebvre@gmail.com>
2019-04-22 03:23:55 -0400

    fix typo in the name of the `acknowledgement` module

--- src/infrastructure.rs
+++ src/infrastructure.rs
@@ -1,13 +1,13 @@
 //! This module provides the logic around the processing of the packet.
 //! Like ordering, sequencing, controlling congestion, fragmentation, and packet acknowledgment.
 
-mod acknowlegement;
+mod acknowledgement;
 mod congestion;
 mod fragmenter;
 
 pub mod arranging;
 
-pub use self::acknowlegement::AcknowledgementHandler;
-pub use self::acknowlegement::SentPacket;
+pub use self::acknowledgement::AcknowledgementHandler;
+pub use self::acknowledgement::SentPacket;
 pub use self::congestion::CongestionHandler;
 pub use self::fragmenter::Fragmentation;
